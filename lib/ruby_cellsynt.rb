4bf7e4096a8e37c1e6cd83da5ff7ece36f3dad7f
Beyar <beyar-123@hotmail.com>
2021-06-23 19:48:47 +0200

    Created function for dynamically adjust message size, also added function for auto formating

---
+++ lib/ruby_cellsynt.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'uri'
+
+# Cellsynt api
+class Cellsynt
+  SMS_CHAR_LIMIT = 160
+  SMS_CONCAT_CHAR_LIMIT = 153
+  SMS_CONCAT_HARD_LIMIT = 918
+
+  attr_accessor :endpoint, :config
+
+  def initialize(username = nil, password = nil)
+    @endpoint = 'https://se-1.cellsynt.net/sms.php'
+
+    @config = {
+      username: username || ENV['cellsyntUser'],
+      password: password || ENV['cellsyntPass'],
+
+      destination: nil,
+      allowconcat: nil, # Values: 1 - 6
+      originator: nil,  # Values: numeric, shortcode & alpha
+      expiry: nil,      # Value: Unix timestamp
+      text: nil,        # Type: String
+      udh: nil,         # Type: Hex
+
+      originatortype: 'numeric', # Values: numeric, shortcode & alpha
+      charset: 'UTF-8',          # Values: ISO-8859-1 & UTF-8, Standard: ISO-8859-1
+      type: 'text',              # Values: text, binary & unicode, Standard: text
+      flash: false,              # Values: true, false
+      class: 1,                  # Values: 0 - 3, Standard: 1
+      pid: 00                    # Type: Hex, Standard: 00
+    }.compact
+  end
+
+  def adjust_concat(message)
+    return nil if message.chars.length == SMS_CONCAT_HARD_LIMIT
+
+    if message.chars.length > SMS_CHAR_LIMIT
+      message_size = message.chars.length
+
+      (1..6).each do |index|
+        if (index * SMS_CONCAT_CHAR_LIMIT) >= message_size
+          return index
+        end
+      end
+    end
+  end
+
+  def format_destination(number)
+    number.delete_prefix('0')
+    number.dup.prepend('0046')
+  end
+
+  def format_originator(number)
+    number.delete_prefix('0')
+    number.dup.prepend('46')
+  end
+
+  def send
+    uri = URI(@endpoint)
+    req = Net::HTTP::Post.new(uri)
+
+    config[:allowconcat] = adjust_concat(@config[:text])
+    config[:destination] = format_destination(@config[:destination])
+    config[:originator] = format_originator(@config[:originator])
+
+    req.set_form_data(@config)
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true, timeout: 300) do |packet|
+      packet.request(req)
+    end
+
+    res.body
+  end
+end
+
+instance = Cellsynt.new('username', 'password')
+
+instance.config[:flash] = true
+instance.config[:text] = 'A' * 160
+instance.config[:originator] = '0711223344'
+instance.config[:destination] = '0711223344'
+
+puts instance.send
